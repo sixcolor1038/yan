dad6c09dbeafafea9ef3a4024c6b2f8823d2f790
sixcolor <sixcolor1038@163.com>
2023-08-06 18:40:28 +0800

    project init

---
+++ yan-demo/src/main/java/com/yan/yandemo/YanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yan.yandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YanDemoApplication.class, args);
+    }
+
+}

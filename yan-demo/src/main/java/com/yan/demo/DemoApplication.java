6f498fdab37ced22c7356589f98078dac3819a07
sixcolor <sixcolor1038@163.com>
2023-08-11 19:06:53 +0800

    project init

--- yan-demo/src/main/java/com/yan/demo/DemoApplication.java
+++ yan-demo/src/main/java/com/yan/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+//@EnableScheduling
 @EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
--- yan-demo/src/main/java/com/yan/demo/demo01/controller/StudentController.java
+++ yan-demo/src/main/java/com/yan/demo/demo01/controller/StudentController.java
@@ -2,9 +2,12 @@ package com.yan.demo.demo01.controller;
 
 import com.yan.demo.demo01.model.Student;
 import com.yan.demo.demo01.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "用户管理")
 @RestController
 @RequestMapping("/student")
 public class StudentController {
@@ -26,7 +29,7 @@ public class StudentController {
     public boolean delete(@PathVariable Long id) {
         return studentService.removeById(id);
     }
-
+    @Operation(summary = "ById")
     @GetMapping("/{id}")
     public Student getById(@PathVariable Long id) {
         return studentService.getById(id);
